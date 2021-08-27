6330322a7c1cd2353555cbcd6cb2b0b94f5776a6
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2021-08-27 08:11:23 +0700

    init project

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('admin')->group(function (){
+    require_once __DIR__ . '/admin.php';
+});
+
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
